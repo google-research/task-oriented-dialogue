0570ca72ca1c48debb11c825987ab8dde55e88d4
Yilei Yang <yileiyang@google.com>
2022-04-22 08:58:30 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 443678849

--- generation/utterance_templates/templates_sanity_test.py
+++ generation/utterance_templates/templates_sanity_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Validates all utterance templates added to this directory.
 
 Verifies that all rows have 2 columns separated by tabs and the number of slot
